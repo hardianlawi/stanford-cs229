92a4c49c656a89f5de82a549fc5b4b3ef8fc8587
hardianlawi <hardianlawi@gmail.com>
2020-10-13 02:06:13 +0000

    complete qns1 partial

--- ps2/src/stability/stability.py
+++ ps2/src/stability/stability.py
@@ -1,23 +1,22 @@
 # Important note: you do not have to modify this file for your homework.
 
-import util
 import numpy as np
+
+from .util import load_csv
 
 
 def calc_grad(X, Y, theta):
     """Compute the gradient of the loss with respect to theta."""
     count, _ = X.shape
 
-    probs = 1. / (1 + np.exp(-X.dot(theta)))
+    probs = 1.0 / (1 + np.exp(-X.dot(theta)))
     grad = (Y - probs).dot(X)
 
     return grad
 
 
-def logistic_regression(X, Y):
-    """Train a logistic regression model."""
+def logistic_regression(X, Y, learning_rate=0.1):
     theta = np.zeros(X.shape[1])
-    learning_rate = 0.1
 
     i = 0
     while True:
@@ -26,22 +25,26 @@ def logistic_regression(X, Y):
         grad = calc_grad(X, Y, theta)
         theta = theta + learning_rate * grad
         if i % 10000 == 0:
-            print('Finished %d iterations' % i)
+            print(f"Finished {i} iterations, grad {grad}, theta {theta}")
         if np.linalg.norm(prev_theta - theta) < 1e-15:
-            print('Converged in %d iterations' % i)
+            print(f"Converged in {i} iterations, theta {theta}")
             break
-    return
+
+        if (i + 1) % (10 * 10000) == 0:
+            break
+
+    return theta
 
 
 def main():
-    print('==== Training model on data set A ====')
-    Xa, Ya = util.load_csv('ds1_a.csv', add_intercept=True)
+    print("==== Training model on data set A ====")
+    Xa, Ya = load_csv("ds1_a.csv", add_intercept=True)
     logistic_regression(Xa, Ya)
 
-    print('\n==== Training model on data set B ====')
-    Xb, Yb = util.load_csv('ds1_b.csv', add_intercept=True)
+    print("\n==== Training model on data set B ====")
+    Xb, Yb = load_csv("ds1_b.csv", add_intercept=True)
     logistic_regression(Xb, Yb)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
